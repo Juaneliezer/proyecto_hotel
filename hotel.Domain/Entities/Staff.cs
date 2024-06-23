baf32318fa0a5735ce325d675257732606de55b4
TheB05 <branderbaez02@gmail.com>
2024-06-23 03:50:30 -0400

    Hotel Domain Changes

---
+++ hotel.Domain/Entities/Staff.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hotel.Domain.Entities
+{
+    public class Staff
+    {
+        public int Id_Staff { get; set; }
+    }
+}
